cf194aad61a754468b9d4c015e1954f9ba182a14
IvanJC <ivan.corica@gmail.com>
2019-06-04 22:51:34 -0300

    Initial commit

---
+++ Assets/LlegoCamion.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LlegoCamion : MonoBehaviour
+{
+    public GameObject nivel1, nivel2, nivel3;
+    GameObject camion;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("LLegaste");
+        camion = other.gameObject;
+        Invoke("SiguienteNivel", 1f);
+    }
+
+    void SiguienteNivel() {
+        if (nivel1.activeSelf)
+        {
+            nivel1.SetActive(false);
+            nivel2.SetActive(true);
+            camion.transform.position = new Vector3(-24,6,24);
+            //Llegada poner en ubi
+        }
+        else if (nivel2.activeSelf)
+        {
+            nivel2.SetActive(false);
+            nivel3.SetActive(true);
+            camion.transform.position = new Vector3(24, 6, 24);
+            //Llegada poner en ubi
+        }
+        else {
+            //Muestra pantalla inicio
+            //
+        }
+    }
+}
---
+++ Assets/MovimientoCamion.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoCamion : MonoBehaviour
+{
+
+    Rigidbody rb;
+    public float velocidad;
+    private bool moving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Arriba
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Euler(-90,-90,0);
+            //this.gameObject.transform.Rotate(0, (-90), 0, Space.World);
+            rb.velocity = new Vector3(0, 0, velocidad);
+        }
+        //Izquierda
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(-90, 180, 0);
+            //this.gameObject.transform.Rotate(0, 180, 0, Space.World);
+            rb.velocity = new Vector3(-velocidad, 0, 0);
+        }
+        //Abajo
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Euler(-90, 90, 0);
+            //this.gameObject.transform.Rotate(0, 90, 0, Space.World);
+            rb.velocity = new Vector3(0, 0, -velocidad);
+        }
+        //Derecha
+        if (Input.GetKey(KeyCode.D)) {
+            transform.rotation = Quaternion.Euler(-90, 0, 0);
+            //this.gameObject.transform.Rotate(0, 0, 0, Space.World);
+            rb.velocity = new Vector3(velocidad, 0, 0);
+        }
+    }
+}
