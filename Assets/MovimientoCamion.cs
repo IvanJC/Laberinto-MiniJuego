a24aaf8c4161e927aa21cf2d30c39621acfe319c
Daniel Fernandez <danicoof@gmail.com>
2019-06-05 17:12:59 -0300

    Nivel 1

--- Assets/MovimientoCamion.cs
+++ Assets/MovimientoCamion.cs
@@ -18,32 +18,32 @@ public class MovimientoCamion : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+
         //Arriba
         if (Input.GetKey(KeyCode.W))
         {
-            transform.rotation = Quaternion.Euler(-90,-90,0);
-            //this.gameObject.transform.Rotate(0, (-90), 0, Space.World);
-            rb.velocity = new Vector3(0, 0, velocidad);
+            transform.rotation = Quaternion.Euler(-90, 90,0);
+            rb.velocity = new Vector3(0, 0, -velocidad);
         }
         //Izquierda
         if (Input.GetKey(KeyCode.A))
         {
-            transform.rotation = Quaternion.Euler(-90, 180, 0);
+            transform.rotation = Quaternion.Euler(-90,  0, 0);
             //this.gameObject.transform.Rotate(0, 180, 0, Space.World);
-            rb.velocity = new Vector3(-velocidad, 0, 0);
+            rb.velocity = new Vector3(velocidad, 0, 0);
         }
         //Abajo
         if (Input.GetKey(KeyCode.S))
         {
-            transform.rotation = Quaternion.Euler(-90, 90, 0);
+            transform.rotation = Quaternion.Euler(-90, -90, 0);
             //this.gameObject.transform.Rotate(0, 90, 0, Space.World);
-            rb.velocity = new Vector3(0, 0, -velocidad);
+            rb.velocity = new Vector3(0, 0, velocidad);
         }
         //Derecha
         if (Input.GetKey(KeyCode.D)) {
-            transform.rotation = Quaternion.Euler(-90, 0, 0);
+            transform.rotation = Quaternion.Euler(-90, 180, 0);
             //this.gameObject.transform.Rotate(0, 0, 0, Space.World);
-            rb.velocity = new Vector3(velocidad, 0, 0);
+            rb.velocity = new Vector3(-velocidad, 0, 0);
         }
     }
 }
---
+++ Assets/movimientoCamara.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientoCamara : MonoBehaviour
+{
+    public GameObject Target;
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - Target.transform.position;
+    }
+
+
+    void FixedUpdate()
+    {
+        transform.position = Target.transform.position + offset;
+    }
+}
